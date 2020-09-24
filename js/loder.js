9f254c72a80017972cf0e97b4cb4c5c5cbb3c1f8
Akash Pradhan <50459609+Akashpradhan-Git@users.noreply.github.com>
2020-09-24 17:11:43 +0530

    Add files via upload

---
+++ js/loder.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('.modal').modal();
+    $('.parallax').parallax();
+    $('.sidenav').sidenav();
+    $('.slider').slider();
+    $('.carousel').carousel({
+        numVisible:7,
+        shift: 70,
+        padding:80,
+    });
+})
+
+function home()
+{
+    URL="www.google.com"
+}
